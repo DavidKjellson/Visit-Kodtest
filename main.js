a21a6f9304fc5a7d2923415489aca16478e28ef6
DavidKjellson <david.kjellson@gmail.com>
2022-01-29 21:32:45 +0100

    Limit width of window

--- main.js
+++ main.js
@@ -5,32 +5,33 @@ const path = require("path");
 let mainWindow
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 578,
+    webPreferences: {
+      nodeIntegration: true
+    },
+  })
 
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `./dist/index.html`),
-            protocol: "file:",
-            slashes: true
-        })
-    );
-    mainWindow.on('closed', function () {
-        mainWindow = null
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, `./dist/index.html`),
+      protocol: "file:",
+      slashes: true
     })
+  );
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 console.log(app);
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') app.quit()
 })
 
 app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+  if (mainWindow === null) createWindow()
 })
